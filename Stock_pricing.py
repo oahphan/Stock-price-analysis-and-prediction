1be3e4524cf506a010e39095ecf20090f2e8b7f1
Oah Phan <phanmyoanh.ba@gmail.com>
2021-11-15 10:45:45 +0700

    Add files via upload

--- Stock_pricing.py
+++ Stock_pricing.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 15 10:32:00 2021
 
-@author: oahphan
-"""
+import numpy as np
+import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Set up End and Start times for data grab
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+VCB = pd.read_csv('excel_vcb.csv',sep=',')
+TCB = pd.read_csv('excel_tcb.csv',sep=',')
+CTG = pd.read_csv('excel_ctg.csv',sep=',')
+
+# VCB
+Cols = ['Ticker','Date','Open','High','Low','Close','Volume']
+
+VCB.columns = Cols
+
+VCB['Date']=pd.to_datetime(VCB['Date'], format='%Y%m%d', errors='ignore')
+
+VCB = VCB.sort_values(by='Date')
+
+VCB = VCB.set_index(['Date'])
+                         
